401e462c48c3b27b7fe2cf40c1a7872faa327eed
Natasha Cox <natashac1014@gmail.com>
2018-07-13 13:36:39 -0500

    renders animal details

--- src/Animal.js
+++ src/Animal.js
@@ -2,8 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+
 export default props => {
-    return (
+    let animal = {}
+
+// Check if the data is in `props.animal`
+if (props.hasOwnProperty("animal")) {
+    animal = props.animal
+
+// If not, data will be in `props.location.state.animal`
+} else {
+    animal = props.location.state.animal
+}
+   return (
         <div className="card" style={{width: `18rem`}}>
             <div className="card-body">
                 <h5 className="card-title">
@@ -11,10 +23,13 @@ export default props => {
                 </h5>
                 <p className="card-text">{props.animal.breed}</p>
                 {
-                    <Link to={`/animals/${props.animal.id}`}
-                          className="card-link">
-                        Details
-                    </Link>
+                    <Link className="card-link"
+                    to={{
+                        pathname: `/animals/${animal.id}`,
+                        state: { animal: animal }
+                    }}>
+                    Details
+                </Link>
                 }
             </div>
         </div>
