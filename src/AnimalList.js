48630614ad55c040acde9c09c73cdf9c54485145
Natasha Cox <natashac1014@gmail.com>
2018-07-16 19:11:21 -0500

    delete works

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -14,12 +14,30 @@ componentDidMount() {
         console.log(animals)
     })
 }
+checkOutAnimal = (animalId) => {
+    // Delete the specified animal from the API
+    fetch(`http://localhost:5002/animals/${animalId}`, {
+        method: "DELETE"
+    })
+    // When DELETE is finished, retrieve the new list of animals
+    .then(() => {
+        // Remember you HAVE TO return this fetch to the subsequenet `then()`
+        return fetch("http://localhost:5002/animals")
+    })
+    // Once the new array of animals is retrieved, set the state
+    .then(a => a.json())
+    .then(animalList => {
+        this.setState({
+            animals: animalList
+        })
+    })
+}
     render() {
         return (
             <React.Fragment>
                     {
                         this.state.animals.map(animal => 
-                        < Animal key={animal.id} animal={animal}>
+                        < Animal key={animal.id} animal={animal} checkOutAnimal={this.checkOutAnimal}>
                         {animal.name}
                         </Animal>
                         )
